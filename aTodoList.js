8bbe2dcabc31f5f21e236de763e822f88790bb2a
SorawitFM <fmzazadds2@gmail.com>
2023-10-02 20:58:40 +0700

    commit 11

--- aTodoList.js
+++ aTodoList.js
@@ -3,10 +3,16 @@ document.getElementById('clickforAdd').addEventListener('click', addList)
 
 
 function addList() {
-    var myInput = document.getElementsByClassName('mytodoList')[0].value
-    var input = document.createTextNode(myInput)
-    var list = document.createElement('li')
-    var unOrderList = document.getElementById('list-container')
-    list.appendChild(input)
-    unOrderList.appendChild(list)
+    var myInput = document.getElementsByClassName('mytodoList')[0]
+    var inputText = myInput.value
+
+    if (inputText !== "") {
+        var input = document.createTextNode(inputText)
+        var list = document.createElement('li')
+        var unOrderList = document.getElementById('list-container')
+        list.appendChild(input)
+        unOrderList.appendChild(list)
+        myInput.value = ""
+    }
+
 }
