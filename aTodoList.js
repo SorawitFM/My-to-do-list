af7b10fce3cc76a8da927305049546599394662e
SorawitFM <fmzazadds2@gmail.com>
2023-10-02 21:19:07 +0700

    commit 12

--- aTodoList.js
+++ aTodoList.js
@@ -10,8 +10,10 @@ function addList() {
         var input = document.createTextNode(inputText)
         var list = document.createElement('li')
         var unOrderList = document.getElementById('list-container')
+        var btnSubtract = document.createElement('button')
         list.appendChild(input)
         unOrderList.appendChild(list)
+        unOrderList.appendChild(btnSubtract)
         myInput.value = ""
     }
 
