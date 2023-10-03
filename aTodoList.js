11cdb27338a24113f228714a414d1c65181aba98
SorawitFM <fmzazadds2@gmail.com>
2023-10-04 01:08:21 +0700

    commit 17

--- aTodoList.js
+++ aTodoList.js
@@ -11,17 +11,19 @@ function addList() {
     var myInput = document.getElementsByClassName('mytodoList')[0]
     var inputText = myInput.value
 
-    if (inputText !== "") {
+    if (inputText !== "") {  //ตรวจสอบว่าต้องมีข้อความถึงจะเพิ่ม List ได้
         var input = document.createTextNode(inputText)
         var list = document.createElement('li')
         var unOrderList = document.getElementById('list-container')
-        var btnSubtract = document.createElement('button')
-        btnSubtract.innerHTML = '\u2716'
+        var btnSubtract = document.createElement('button') //สร้างปุ่มกากบาท
+        btnSubtract.innerHTML = '\u2716'  //ใส่สัญลักษณ์กากบาท
+
         list.appendChild(input)
         list.appendChild(btnSubtract)
         unOrderList.appendChild(list)
         myInput.value = ""
     }
+
     btnSubtract.addEventListener('click', removeList)
     function removeList() {
         list.remove()
