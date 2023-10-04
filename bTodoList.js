eb762e7eb0bb28a753d73438db97a3aa9b043162
SorawitFM <fmzazadds2@gmail.com>
2023-10-04 17:23:21 +0700

    commit 19

---
+++ bTodoList.js
@@ -0,0 +1,31 @@
+document.getElementById('clickforAdd').addEventListener('click', addList)
+
+function handleEnter(event) {
+    if (event.key === 'Enter') {
+        addList()
+    }
+}
+
+
+function addList() {
+    var myInput = document.getElementsByClassName('mytodoList')[0]
+    var inputText = myInput.value
+
+
+    if (inputText !== "") {
+        var listItem = document.createElement('li')
+        var listContainer = document.getElementById('list-container')
+        var input = document.createTextNode(inputText)
+        var removeButton = document.createElement('button')
+        removeButton.innerHTML = '\u2716'
+        listItem.appendChild(input)
+        listItem.appendChild(removeButton)
+        listContainer.appendChild(listItem)
+        myInput.value = ""
+    }
+
+    removeButton.addEventListener('click', removeList)
+    function removeList() {
+        listItem.remove()
+    }
+}
