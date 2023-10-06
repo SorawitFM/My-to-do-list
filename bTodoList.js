28e8cfc436981d54db91f1ba46a0a0057e87d232
SorawitFM <fmzazadds2@gmail.com>
2023-10-06 12:51:13 +0700

    commit 23

--- bTodoList.js
+++ bTodoList.js
@@ -1,4 +1,5 @@
 document.getElementById('clickforAdd').addEventListener('click', addList)
+window.addEventListener('load', loadListFromLocalStorage)
 
 function handleEnter(event) {
     if (event.key === 'Enter') {
@@ -31,12 +32,40 @@ function addList() {
     }
 
 
-    function removeList() {
-        var listItem = this.parentNode
-        var inputText = listItem.firstChild.textContent
-        var ListInStorage = localStorage.getItem('List') || ''
-        ListInStorage = ListInStorage.replace(inputText + '\n', '')
-        localStorage.setItem('List', ListInStorage)
-        listItem.remove()
+
+}
+
+function loadListFromLocalStorage() {
+    var ListInStorage = localStorage.getItem('List') || ''
+    var listfromStorage = ListInStorage.split('\n')
+
+    var listContainer = document.getElementById('list-container')
+    listContainer.innerHTML = ''
+
+    for (var i = 0; i < listfromStorage.length; i++) {
+        var inputText = listfromStorage[i].trim();
+        if (inputText !== "") {
+            var input = document.createTextNode(inputText)
+            var listItem = document.createElement('li')
+            var removeButton = document.createElement('button')
+            removeButton.innerHTML = '\u2716'
+
+            listItem.appendChild(input)
+            listItem.appendChild(removeButton)
+            listContainer.appendChild(listItem)
+
+
+
+            removeButton.addEventListener('click', removeList)
+        }
+
     }
+}
+function removeList() {
+    var listItem = this.parentNode
+    var inputText = listItem.firstChild.textContent
+    var ListInStorage = localStorage.getItem('List') || ''
+    ListInStorage = ListInStorage.replace(inputText + '\n', '')
+    localStorage.setItem('List', ListInStorage)
+    listItem.remove()
 }
